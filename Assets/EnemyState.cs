996302025f0edc592b6f1030a49afb82cd7d143f
zhongba01 <zhongba01@mail.jj.cn>
2025-06-24 21:00:58 +0800

    添加可移动的无限背景，跟随相机，墙体，初步添加敌人脚本

---
+++ Assets/EnemyState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState
+{
+    
+    
+    protected virtual void Start()
+    {
+        
+    }
+    
+    protected virtual void Update()
+    {
+        
+    }
+}
